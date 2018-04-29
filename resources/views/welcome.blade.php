9a7b95ea428d043de104aba1be2538adb0370275
jessonfoo <jessonfoo@gmail.com>
2018-04-29 14:26:49 -0700

    Lol omg so much front end stuff
    Signed-off-by: jessonfoo <jessonfoo@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,6 @@
 @show
 
 <div class="container">
-	<p> test </p>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<a class="navbar-brand" href="#">
 			<img src="{{asset('/images/logo.png')}}" width="50" height="50" class="d-inline-block align-top" alt="">
@@ -32,33 +31,6 @@
 			<span class="navbar-toggler-icon">
 			</span>
 		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#">Team</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#">About</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#">Services</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#">Pricing</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#">Contact</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
-	<nav class="navbar navbar-light bg-light">
-		<a class="navbar-brand" href="#">
-			<img src="/assets/images/logo.png" width="50" height="50" class="d-inline-block align-top" alt="">
-		</a>
 		<div class="collapse navbar-collapse" id="navbarNav">
 			<ul class="navbar-nav">
 				<li class="nav-item active">
